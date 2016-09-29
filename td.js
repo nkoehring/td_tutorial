015433cd0b4911cb9f23cd237c19486a6f9207f7
Norman <n@koehr.in>
2016-09-29 20:30:26 +0200

    deadly clicks
    a simple enemy component

--- td.js
+++ td.js
@@ -2,6 +2,43 @@ class Game {
   constructor(w, h) {
     Crafty.init(w, h)
     Crafty.background("black")
+    Crafty.c("Enemy", {
+      required: "2D, Canvas, Color, Mouse",
+
+      events: {
+        Click (evt) {
+          this.hit(1)
+        }
+      },
+
+      init () {
+        this.x = 50
+        this.y = 50
+        this.w = 20
+        this.h = 20
+        this.hp = 1
+        this.color("#FF0000")
+      },
+
+      level (level) {
+        this.hp = this.hp * level
+        return this
+      },
+
+      hit (damage) {
+        this.hp -= damage
+        Crafty.log("hp is now", this.hp)
+
+        if (this.hp <= 0) {
+          Crafty.log("Aw, now I have to wait until the next round!")
+          this.destroy()
+        }
+
+        return this
+      }
+    })
+
+    Crafty.e("Enemy").level(10)
   }
 }
 
